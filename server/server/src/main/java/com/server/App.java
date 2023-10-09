8b12f2974b9d839bc4810f73f53eeb00baa93673
YvngLore <socci.lorenzo@itismeucci.com>
2023-10-09 09:40:14 +0200

    Server concluso

--- server/server/src/main/java/com/server/App.java
+++ server/server/src/main/java/com/server/App.java
@@ -1,5 +1,10 @@
 package com.server;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 /**
  * Hello world!
  *
@@ -8,6 +13,25 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            ServerSocket server = new ServerSocket(3000);
+            System.out.println("Server inizializzato");
+
+            Socket socket = server.accept();
+            System.out.println("Un client si è connesso!");
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+
+            String ricevuta = input.readLine();
+            System.out.println("Stringa ricevuta dal client: " + ricevuta);
+            output.writeBytes(ricevuta.toUpperCase() + "\n");
+
+            server.close();
+            socket.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Errore durante l'istanzia del server!");
+        }
     }
 }
