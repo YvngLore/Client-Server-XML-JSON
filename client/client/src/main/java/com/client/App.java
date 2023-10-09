2666797d580123c91b3f6e423d57cd561fbbfb72
YvngLore <socci.lorenzo@itismeucci.com>
2023-10-09 09:53:15 +0200

    Client concluso

--- client/client/src/main/java/com/client/App.java
+++ client/client/src/main/java/com/client/App.java
@@ -1,5 +1,10 @@
 package com.client;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            Socket socket = new Socket("localhost", 3000);
+            System.out.println("Socket inizializzato");
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+
+            output.writeBytes("Ciao server" + "\n");
+            System.out.println("Il server ha risposto: " + input.readLine());
+
+            socket.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("ERRORE GENERICO");
+        }
     }
 }
